3d31b015227278adef03c741fd2ef6cd10e745bc
tox-in <habimanat78@gmail.com>
2025-02-25 22:01:51 +0200

    Jwt Auth Filter content

--- src/main/java/com/codes/study/security/JwtAuthenticationFilter.java
+++ src/main/java/com/codes/study/security/JwtAuthenticationFilter.java
@@ -1,4 +1,29 @@
 package com.codes.study.security;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.constraints.NotNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private final JwtAuthenticationProvider jwtAuthenticationProvider;
+    private final UserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain
+            ) throws ServletException, IOException {
+        final String authHeader
+    }
 }
